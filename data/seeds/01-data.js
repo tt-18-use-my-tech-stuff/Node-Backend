72ec1b9332a6c1d1e2e58c7c073e851513ac30af
Matthew Ellis <mbellistx@gmail.com>
2021-04-26 13:00:03 -0500

    added a user to seed

--- data/seeds/01-data.js
+++ data/seeds/01-data.js
@@ -1,9 +1,9 @@
+const bcrypt = require('bcryptjs')
 // DO NOT CHANGE THIS FILE
+// I'm changing it :) if it breaks you can blame Matthew
 exports.seed = async function (knex) {
   await knex('users').truncate();
   await knex('users').insert([
-    {
-      username: 'foo'
-    }
+    { username: 'bobby', password: bcrypt.hashSync('aoeu', 8) }
   ]);
 }
