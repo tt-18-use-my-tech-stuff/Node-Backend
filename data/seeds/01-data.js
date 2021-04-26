0f35f844a05647f183a6033306b3fcb3edb035a8
Matthew Ellis <mbellistx@gmail.com>
2021-04-26 16:06:40 -0500

    changed truncate to del to make postgres happy

--- data/seeds/01-data.js
+++ data/seeds/01-data.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // DO NOT CHANGE THIS FILE
 // I'm changing it :) if it breaks you can blame Matthew
 exports.seed = async knex => {
-  await knex('users').truncate();
+  await knex('users').del();
   return knex('users').insert([
     { username: 'bobby', password: bcrypt.hashSync('aoeu', 8) }
   ]);
