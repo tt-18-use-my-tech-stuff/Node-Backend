1f89ae27f5d8561edc9ea9a8b9ea058ebbb62acf
Jonathan Fox <FoxJonathanP@gmail.com>
2021-04-26 11:09:58 -0700

    added model functions

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -2,23 +2,30 @@
 const db = require("../../data/dbconfig");
 
 const find = () => {
-
+	return db('items');
 };
+
+const findBy = (filter) => {
+	return db('items').where(filter).first();
+}
 
 const findById = (item_id) => {
-
+	return findBy({ item_id });
 };
 
-const add = (item) => {
-
+const add = async (item) => {
+	const [ createdItemId ] = await db("items").insert(item);
+	return findById(createdItemId);
 };
 
-const editById = (item_id, item) => {
-
+const editById = async (item_id, item) => {
+	const [ editedItemId ] = await db("items").where({ item_id }).update(item);
+	return findById(editedItemId);
 };
 
-const deleteById = (item_id) => {
-
+const deleteById = async (item_id) => {
+	const [ deletedItemId ] = await db("items").where({ item_id }).del();
+	return deletedItemId;
 };
 
 module.exports = {
@@ -27,4 +34,4 @@ module.exports = {
 	add,
 	editById,
 	deleteById
-}
+};
