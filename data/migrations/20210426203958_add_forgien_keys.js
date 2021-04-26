f731910a2a0fb4feeea2a451a7f6a7fd1d4051d4
Matthew Ellis <mbellistx@gmail.com>
2021-04-26 16:01:30 -0500

    fixed type in migration

--- data/migrations/20210426203958_add_forgien_keys.js
+++ data/migrations/20210426203958_add_forgien_keys.js
@@ -3,7 +3,7 @@ exports.up = knex => {
   return knex.schema.table('items', tbl => {
     tbl.integer('request_id')
       .unsigned()
-      .references('request.request_id')
+      .references('requests.request_id')
       .onDelete('CASCADE')
       .onUpdate('CASCADE')
   })
