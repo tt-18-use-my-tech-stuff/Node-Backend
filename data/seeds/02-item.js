effb39646bda6d66db785e5f744f551fd713a0d6
Matthew Ellis <mbellistx@gmail.com>
2021-04-27 10:53:22 -0500

    fixed seed and migration?

--- data/seeds/02-item.js
+++ data/seeds/02-item.js
@@ -1,6 +1,6 @@
 
 exports.seed = async knex => {
-  await knex('items').truncate();
+  await knex('items').del();
   return knex('items').insert([
     { item_id: 1, item_name: 'Standing Desk', item_description: "It stands! it's a desk!", owner_id: 1 },
     { item_id: 2, item_name: 'Headphones', item_description: "Guaranteed 88% germ-free!", owner_id: 1 },
