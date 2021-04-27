51665039e2bb85944d1c69f13b0cdddc238cebdd
¨senac2106 <emiliosneac7@gmail.com>
2021-04-27 08:25:51 -0700

    sign up form

---
+++ sign up/p.js
@@ -0,0 +1,5 @@
+const cars = [
+    {name:'tesla', model:'y', price:500},
+    {name:'bmw', model:'zeta', price:400}
+
+]
---
+++ sign up/sign up.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export default function Form() {
+    const [Valu, setFormValues] = useState({
+    });
+    
+    const submitForm = event => {
+        event.preventDefault();
+        const newUser = {
+            name: Valu.myName.trim(),
+            email: Valu.myEmail.trim(),
+            password: Valu.myPassword.trim()
+        }
+        setUsers(users.concat(newUser))
+        setFormValues(initialFormValues)
+        console.log('Submitted');
+    };
+
+    const onChanges = event => {
+        console.log('Changed', event.target.value);
+        const {name, value} = event.target
+        setFormValues({...Valu, [name]: value,
+        });
+    };
+
+    return (
+        <form onSubmit={submitForm}>
+            <label>Name: 
+                <input 
+                    name='myName'
+                    type='text'
+                    onChange={onChanges}
+                    value={Valu.myName}
+                    
+                />
+            </label>
+            <label>Email:
+                <input 
+                    type='text'
+                    name='myEmail'
+                    onChange={onChanges}
+                    value={Valu.myEmail}
+                    
+                />
+            </label>
+            <label>Password:
+                <input 
+                    type='text'
+                    name='myPassword'
+                    onChange={onChanges}
+                    value={Valu.myPassword}
+                    
+                />
+            </label>
+            <label> Confirm password:
+            <input
+            type='text'
+            name='myPassword'
+            onChange={onChanges}
+            value={Valu.myPassword}
+            />
+            </label>
+            <label>SubmitBtn
+
+</label>
+</form>
+)
+} 
