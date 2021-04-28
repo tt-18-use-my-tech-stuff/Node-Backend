88eb201023828ceb6a90c96cb4c72ebefd4114ec
Matthew Ellis <mbellistx@gmail.com>
2021-04-28 16:10:02 -0500

    added more producton tests

--- api/production-server.test.js
+++ api/production-server.test.js
@@ -81,7 +81,7 @@ describe('/api/auth routes', () =>{
   })
 })
 
-describe.only('/api/items', () => {
+describe('/api/items', () => {
 
   let headers = {}
   const path = '/items'
@@ -157,13 +157,120 @@ describe.only('/api/items', () => {
     it('can delete items', async () => {
       let res
       res = await axios.post(base_url + path, testItem, headers)
-      console.log('delete post res', res)
       const item_id = res.data.item_id
       res = await axios.delete(base_url + path + '/' + item_id, headers).catch( err => console.log(162, err.response))
       expect(res.status).toBe(200)
       res = await axios.get(base_url + path + '/' + item_id, headers).catch( err => err.response)
       expect(res.status).toBe(404)
       expect(res.data.message).toBe(`No item found with id ${item_id}.`)
+    })
+  })
+})
+
+describe('/api/requests', () => {
+
+  let headers = {}
+  const user = { username: 'bobby', password: 'aoeu' }
+
+  beforeAll(async () => {
+    let res = await axios.post(base_url + '/auth/login', user)
+    headers = { headers: { authorization: res.data.token } }
+  })
+
+  describe.skip('get api/account/requests/owned', () => {
+    const path = '/api/account/requests/owned'
+  
+    it('fails no token', async () => {
+      let res = await axios.get(base_url + path, {}).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails invalid token', async () => {
+      let res = await axios.get(base_url + path, {authorization: 'invalid token' }).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it.todo('gets on good token fails on respons "you have no requests"')
+    it('gets on good token', async () => {
+      let res = await axios.get(base_url + path, headers).catch(err => err.response)
+      expect(res.status).toBe(200)
+      expect(Array.isArray(res.data)).toBe(true)
+    })
+  })
+  
+  describe.skip('get api/account/requests', () => {
+    const path = '/api/account/requests'
+  
+    it('fails no token', async () => {
+      let res = await axios.get(base_url + path, {}).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails invalid token', async () => {
+      let res = await axios.get(base_url + path, {authorization: 'invalid token' }).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it.skip('gets on good token', async () => {
+      let res = await axios.get(base_url + path, headers).catch(err => err.response)
+      expect(res.status).toBe(200)
+      console.log(53, res.data) // don't know why Array.isArray is returning false
+      expect(Array.isArray(res.data)).toBe(true)
+    })
+  })
+  
+  describe('post api/requests', () => {
+    const path = '/requests'
+    const newRequest = { item_id: 4 }
+  
+    it('fails no token', async () => {
+      let res = await axios.post(base_url + path, newRequest, {}).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails invalid token', async () => {
+      let res = await axios.post(base_url + path, newRequest, {authorization: 'invalid token' }).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails no id', async () => {
+      let res = await axios.post(base_url + path, {}, headers).catch(err => err.response)
+      expect(res.status).toBe(400)
+    })
+    it('fails invalid id', async () => {
+      let res = await axios.post(base_url + path, {item_id: 1000000}, headers).catch(err => err.response)
+      console.log(res)
+      expect(res.status).toBe(404)
+    })
+    it('returns new request', async () => {
+      let res = await axios.post(base_url + path, newRequest, headers).catch(err => err.response)
+      expect(res.status).toBe(201)
+      expect(res.data).toHaveProperty('request_id')
+      expect(res.data).toHaveProperty('item_id')
+      expect(res.data).toHaveProperty('owner_id')
+      expect(res.data).toHaveProperty('renter_id')
+      expect(res.data).toHaveProperty('status', 'pending')
+    })
+  
+  })
+  
+  describe('get api/requests/:id', () => {
+    const path = '/requests/'
+  
+    it('fails no token', async () => {
+      let res = await axios.get(base_url + path + '1', {}).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails invalid token', async () => {
+      let res = await axios.get(base_url + path + '1', {headers: {authorization: 'invalid token'} }).catch(err => err.response)
+      expect(res.status).toBe(401)
+    })
+    it('fails invalid id', async () => {
+      let res = await axios.get(base_url + path + '10000000', headers).catch(err => err.response)
+      expect(res.status).toBe(404)
+    })
+    it('gets request', async () => {
+      let res = await axios.get(base_url + path + '1', headers).catch(err => err.response)
+      expect(res.status).toBe(200)
+      expect(res.data).toHaveProperty('request_id')
+      expect(res.data).toHaveProperty('item_id')
+      expect(res.data).toHaveProperty('owner_id')
+      expect(res.data).toHaveProperty('renter_id')
+      expect(res.data).toHaveProperty('status')
     })
   })
 })
--- api/requests/requests-router.js
+++ api/requests/requests-router.js
@@ -22,6 +22,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:request_id', checkRequestExists, (req, res) => {
+  console.log('request router', req.params)
   res.status(200).json(req.request);
 });
 
